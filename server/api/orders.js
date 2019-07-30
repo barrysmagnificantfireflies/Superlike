07a2cf3ce312c0f21b753339cd722fd7bf63d5e3
Molly Lupton <molly.lupton@gmail.com>
2019-07-30 12:15:20 -0400

    router for orders

--- server/api/orders.js
+++ server/api/orders.js
@@ -20,11 +20,11 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const newOrder = await Order.create(req.body)
-//     res.send(newOrder)
-//   } catch (error) {
-//     next(error)
-//   }
-// })
+router.post('/', async (req, res, next) => {
+  try {
+    const newOrder = await Order.create(req.body)
+    res.send(newOrder)
+  } catch (error) {
+    next(error)
+  }
+})
