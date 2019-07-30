961c7e0c9b428cb2b23c573c6aaffd6ba6f44de8
danny <dli0906@gmail.com>
2019-07-30 12:37:43 -0400

    added item express handler

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/item', require('./item')) //need to change this to items to keep consistency -- whoops
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
