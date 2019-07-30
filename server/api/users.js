6b6d62d2f9f7513fe243b5079fa0b9405d40ed12
sagarvadalia <sv344@cornell.edu>
2019-07-30 15:32:57 -0400

    user routes following CRUD

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,35 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+router.post('/', async (req, res, next) => {
+  try {
+    const createdUser = await User.create(req.body)
+    res.json(createdUser)
+  } catch (error) {
+    next(error)
+  }
+})
+router.put('/', async (req, res, next) => {
+  try {
+    const putUser = await User.update(req.body)
+    res.json(putUser)
+  } catch (error) {
+    next(error)
+  }
+})
+router.delete('/', async (req, res, next) => {
+  try {
+    const destroyedUser = await User.destroy(req.body)
+    res.json(destroyedUser)
+  } catch (error) {
+    next(error)
+  }
+})
