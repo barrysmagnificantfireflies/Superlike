c15f3161843cd4b3f902647ac6ff6640d68d4134
danny <dli0906@gmail.com>
2019-07-31 11:48:10 -0400

    currently-getting-a-500-error-but-thats-ok-maybe

---
+++ client/components/cart.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getCartThunk} from './../store/cart'
+
+class Cart extends Component {
+  componentDidMount() {
+    this.props.getCart()
+  }
+
+  render() {
+    const cart = this.props.cart
+    console.log(cart)
+    return (
+      <div>
+        <h1>Hello</h1>
+        {cart.length === 0 ? (
+          'Cart is empty'
+        ) : (
+          <div>You have {cart.length} items in cart. </div>
+        )}
+        <ul>
+          {cart.map(item => (
+            <li key={item.id}>
+              <button>X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  }
+}
+
+const mapDispatchToProps = () => {
+  return dispatch => {
+    return {
+      getCart: () => dispatch(getCartThunk())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
