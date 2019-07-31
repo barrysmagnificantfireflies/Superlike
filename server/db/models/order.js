003f5a4ec4e91a8162e9609ce458d86922621305
sagarvadalia <sv344@cornell.edu>
2019-07-31 16:23:17 -0400

    try catches added to some class methods on order

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -10,23 +10,31 @@ const Order = db.define('order', {
 
 //this prototype method finds purchase history for individual users, taking in a userId as a paramater, returning an array of purchase objects
 Order.findPurchaseHistory = async function(userId) {
-  const purchase = await this.findAll({
-    where: {
-      userId,
-      isPurchased: true
-    }
-  })
-  return purchase
+  try {
+    const purchase = await this.findAll({
+      where: {
+        userId,
+        isPurchased: true
+      }
+    })
+    return purchase
+  } catch (error) {
+    console.error(error)
+  }
 }
 // this takes an order and finds the price of all items * their quantity
 // not certain this makes sense
 Order.findTotalPrice = async function(order) {
-  const itemArr = order.getItems()
-  let price = 0
-  for (let i = 0; i < itemArr.length; i++) {
-    price += parseInt(i[price] * i[quantity])
+  try {
+    const itemArr = await order.getItems()
+    let price = 0
+    for (let i = 0; i < itemArr.length; i++) {
+      price += parseInt(i[price] * i[quantity])
+    }
+    return price
+  } catch (error) {
+    console.error(error)
   }
-  return price
 }
 
 module.exports = Order
