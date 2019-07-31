6a187f334974a5b77a9b1bbaaea9b3233d550d3a
sagarvadalia <sv344@cornell.edu>
2019-07-31 10:52:09 -0400

    account page component with user info

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, AccountPage} from './components'
 import {me} from './store'
 
 /**
@@ -24,9 +24,10 @@ class Routes extends Component {
         <Route path="/signup" component={Signup} />
         <Route path="/home" component={UserHome} />
         <Route path="/cart" component={Login} />
+        <Route path="/account" component={AccountPage} />
         {isLoggedIn && (
           <Switch>
-            <Route path="/account" component={Login} />
+            <Route path="/account" component={AccountPage} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
--- client/store/user.js
+++ client/store/user.js
@@ -23,6 +23,7 @@ const removeUser = () => ({type: REMOVE_USER})
  */
 export const me = () => async dispatch => {
   try {
+    console.log('here')
     const res = await axios.get('/auth/me')
     dispatch(getUser(res.data || defaultUser))
   } catch (err) {
