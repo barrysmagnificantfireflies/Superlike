54ed64f0839c84a12fde173f9471d76b5c449cdd
Andrew Culkin <andrewsculkin@gmail.com>
2019-08-05 19:19:21 -0400

    updated the edit user page

--- client/components/account-page.js
+++ client/components/account-page.js
@@ -3,6 +3,16 @@ import {me} from './../store/user'
 import React from 'react'
 import EditAccountPage from './editAccountPage'
 
+//material UI imports
+import {makeStyles} from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
 class AccountPage extends React.Component {
   componentDidMount() {
     this.props.getAll()
@@ -10,13 +20,29 @@ class AccountPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello {this.props.user.email}</h1>
-        <img src={this.props.user.imageUrl} />
-        <EditAccountPage
-          email={this.props.user.email}
-          imageUrl={this.props.user.imageUrl}
-          id={this.props.user.id}
-        />
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              height={140}
+              image={this.props.user.imageUrl}
+              title="Profile Picture"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Hello {this.props.user.email}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                If you would like to edit your profile information, please do so
+                below:
+              </Typography>
+            </CardContent>
+            <EditAccountPage
+              email={this.props.user.email}
+              imageUrl={this.props.user.imageUrl}
+              id={this.props.user.id}
+            />
+          </CardActionArea>
+        </Card>
       </div>
     )
   }
